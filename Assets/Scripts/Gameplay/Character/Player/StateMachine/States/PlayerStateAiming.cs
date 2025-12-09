27282ddb3f69656c8f41e93cd12181ef50c8e09c
Qiao Xu <sleeepd@gmail.com>
2025-12-08 19:57:37 -0700

    Added Aiming rotate

--- Assets/Scripts/Gameplay/Character/Player/StateMachine/States/PlayerStateAiming.cs
+++ Assets/Scripts/Gameplay/Character/Player/StateMachine/States/PlayerStateAiming.cs
@@ -13,6 +13,35 @@ public class PlayerStateAiming : PlayerState
         StateMachin.Player.Input.AimingCanceled += HandleAimingCanceled;
     }
 
+    public override void Update()
+    {
+        base.Update();
+        var camera = Camera.main;
+        if (camera == null)
+        {
+            return;
+        }
+
+        var playerTransform = StateMachin.Player.transform;
+        var ray = camera.ScreenPointToRay(StateMachin.Player.Input.PointerPosition);
+        var aimPlane = new Plane(Vector3.up, new Vector3(0f, playerTransform.position.y, 0f));
+
+        if (!aimPlane.Raycast(ray, out var enter))
+        {
+            return;
+        }
+
+        var target = ray.GetPoint(enter);
+        var direction = target - playerTransform.position;
+        direction.y = 0f;
+
+        if (direction.sqrMagnitude < 0.0001f)
+        {
+            return;
+        }
+
+        StateMachin.Player.Movement.Rotate(direction);
+    }
 
 
     public override void OnExit()
@@ -24,6 +53,9 @@ public class PlayerStateAiming : PlayerState
 
     void HandleAimingCanceled()
     {
-        StateMachin.ChangeState<PlayerStateIdle>();
+        var moveDir = StateMachin.Player.Input.MoveDirection;
+        StateMachin.ChangeState(moveDir.sqrMagnitude > 0f
+            ? typeof(PlayerStateRunning)
+            : typeof(PlayerStateIdle));
     }
 }
